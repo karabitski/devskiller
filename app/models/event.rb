d141cdc6b1637fec4b3919c009e12091a92f0f6e
Andrei <a.karabitski@gmail.com>
2023-07-05 14:39:11 +0200

    replace Enum gem with custom code;
    it was unexpected to me I can’t use external libraries couple minutes before my time ended, as I haven’t found any mentions about that restriction

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,16 +4,13 @@ class Event < ApplicationRecord
   validates :employee_id, presence: true, numericality: true
   validates :timestamp, presence: true
   validates :kind, presence: true
+  enum :kind, { in: 0, out: 1 }
 
   scope :between, ->(start_date, end_date) { where(timestamp: start_date.beginning_of_day..end_date.end_of_day) }
   scope :for_employee, -> (id) { where employee_id: id }
 
   def self.modify_timestamp_and_save!(params)
     attrs = params.merge(timestamp: Time.at(params[:timestamp].to_i))
-    kind = 0 if params[:kind].to_s == 'in'
-    kind = 1 if params[:kind].to_s == 'out'
-    attrs.merge!(kind: kind)
     create! attrs
   end
-
 end
