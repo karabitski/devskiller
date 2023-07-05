9ebf3b280e7e4392ada6218bc2052a4e2a04fa06
Andrei <a.karabitski@gmail.com>
2023-07-05 13:59:11 +0200

    trrr

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class Event < ApplicationRecord
-  extend Enumerize
+  # extend Enumerize
   validates :employee_id, presence: true, numericality: true
   validates :timestamp, presence: true
-
-  enumerize :kind, in: { in: 0, out: 1 }
+  validates :kind, presence: true
+  # enumerize :kind, in: { in: 0, out: 1 }
   # validates :kind, required: true  # validation inside enumerize
 
   scope :between, ->(start_date, end_date) { where(timestamp: start_date.beginning_of_day..end_date.end_of_day) }
@@ -13,6 +13,7 @@ class Event < ApplicationRecord
 
   def self.modify_timestamp_and_save!(params)
     attrs = params.merge(timestamp: Time.at(params[:timestamp].to_i))
+    attrs = params.merge(kind: (params[:kind] == 'in' : 0 : 1))
     create! attrs
   end
 
